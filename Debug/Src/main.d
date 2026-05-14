bfad2a28750cc4cba16f4aab61629f0ae5244d8b
horvathd2 <horvath.danihd@gmail.com>
2026-05-14 16:35:03 +0300

    initial commit

---
+++ Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Include/core_cm3.h \
+ D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Include/cmsis_version.h \
+ D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Include/cmsis_compiler.h \
+ D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Include/cmsis_gcc.h \
+ D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Include/core_cm3.h:
+D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Include/cmsis_version.h:
+D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Include/cmsis_compiler.h:
+D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Include/cmsis_gcc.h:
+D:/STM32CubeIDE/workspace_1.19.0/STM32F103C8T6_BAREMETAL/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
